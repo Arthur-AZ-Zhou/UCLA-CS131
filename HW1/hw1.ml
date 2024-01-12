5ba702341abb61d38145984c8346a7fcbf43cb1c
Arthur Zhou <arthuracct@gmail.com>
2024-01-12 15:50:10 -0800

    first commit

---
+++ HW1/hw1.ml
@@ -0,0 +1,7 @@
+let my_value = 2+3;;
+let () = print_int my_value;
+print_newline()
+
+(* let subset_test0 = subset [] [1;2;3]
+let subset_test1 = subset [3;1;3] [1;2;3]
+let subset_test2 = not (subset [1;3;7] [4;1;3]) *)
