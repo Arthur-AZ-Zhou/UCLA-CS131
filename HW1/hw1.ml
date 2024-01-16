0c0f4be3a8c4c339c9335263c8995a462ccc3f70
Arthur Zhou <arthuracct@gmail.com>
2024-01-16 15:46:57 -0800

    Halfway done with first OCaml project. Learned how to implement basic subset functions using recursion

--- HW1/hw1.ml
+++ HW1/hw1.ml
@@ -1,7 +1,45 @@
-let my_value = 2+3;;
-let () = print_int my_value;
-print_newline()
+open List
 
-(* let subset_test0 = subset [] [1;2;3]
-let subset_test1 = subset [3;1;3] [1;2;3]
-let subset_test2 = not (subset [1;3;7] [4;1;3]) *)
+(*Part 1*)
+let rec is_element x lst = match lst with
+    | [] -> false
+    | head :: tail -> head = x || is_element x tail;;
+
+let rec subset a b = match a with
+    | [] -> true
+    | head :: tail -> 
+        if (is_element head b == false) 
+            then false
+        else 
+            subset tail b;;
+
+(*Part 2*)
+let equal_sets a b =
+    if (subset a b && subset b a == true) 
+        then true
+    else 
+        false;;
+
+(*Part 3*)
+let rec set_union a b = match a with 
+    | [] -> b 
+    | head :: tail -> 
+        if (is_element head b == true)
+            then set_union tail b
+        else 
+            set_union tail (head :: b);;
+
+(*Part 4*)
+let rec set_all_union a = match a with (*returns a list*)
+    | [] -> 
+        (* Printf.printf "Empty list\n"; *)
+        []
+    | [x] -> x    
+    | head :: tail -> set_all_union tail@(set_union head (List.hd tail)) ;;
+
+(*Part 5*)
+(*It's actually impossible to write this function due to the way how the paradox works. Let's look at set S of all sets that don't contain themselves. If S 
+contains itself then it should not be in the set S but if it doesn't contain itself then it should be in the set S but the fact that it is in set S is a 
+contradition. Due to this paradox OCaml doesn't allow sets that contains themselves*)
+
+(*Part 6*)
